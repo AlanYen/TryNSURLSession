140ebc3bea65a3b5ae878aba6a30ef0563538fb9
AlanYen <alanyen.mobile@gmail.com>
2015-09-15 15:21:59 +0800

    Initial Commit

---
+++ TryNSURLSession/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TryNSURLSession
+//
+//  Created by AlanYen on 2015/9/15.
+//  Copyright (c) 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TryNSURLSession/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TryNSURLSession
+//
+//  Created by AlanYen on 2015/9/15.
+//  Copyright (c) 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
